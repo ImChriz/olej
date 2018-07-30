15a9021b5681159b32f3b880a6d85b6863cfdef1
Chriz <animen@go2.pl>
2018-07-30 15:39:39 +0200

    start

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/admin/');
+define('HTTP_CATALOG', 'http://127.0.0.1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/admin/');
+define('HTTPS_CATALOG', 'http://127.0.0.1/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123123ab');
+define('DB_DATABASE', 'olejowezdrowie');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
